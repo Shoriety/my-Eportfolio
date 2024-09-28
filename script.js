ff0a61290d8f8f7aff9855e3d38699f9b2b4a4e4
Jess <cjc47761@gmail.com>
2024-09-28 21:14:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// Toggle Menu Icon and Navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/*==================== Scroll Sections Active Link ==========================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        // Activate the corresponding nav link on scroll
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // Sticky Navbar on Scroll
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Hide the menu icon and navbar on scroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+//scroll reveal
+ScrollReveal ({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
